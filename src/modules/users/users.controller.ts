7bd10ab591f3c6507bb0c7ab7a380dbab683326d
rodrigojsdeveloper <rodrigojsdeveloper@gmail.com>
2025-10-04 12:29:16 -0300

    feat: add route to add user to group

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AddUserToGroupDto } from './dto/add-user-to-group.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() dto: CreateUserDto) {
+  create(@Body() dto: CreateUserDto) {
     return this.usersService.create(dto);
+  }
+
+  @Post(':id/groups')
+  @HttpCode(204)
+  addUserToGroup(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() dto: AddUserToGroupDto,
+  ) {
+    return this.usersService.addUserToGroup(id, dto);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,16 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ClosureEntity } from 'src/entities/closure.entity';
 import { NodeEntity, NodeType } from 'src/entities/node.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AddUserToGroupDto } from './dto/add-user-to-group.dto';
 
 @Injectable()
 export class UsersService {
@@ -37,5 +44,68 @@ export class UsersService {
     });
 
     return user;
+  }
+
+  async addUserToGroup(userId: string, dto: AddUserToGroupDto) {
+    const { groupId } = dto;
+
+    const user = await this.nodeRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException(`User ${userId} not found`);
+    }
+    if (user.type !== NodeType.USER) {
+      throw new BadRequestException(`Node ${userId} is not a USER`);
+    }
+
+    const group = await this.nodeRepository.findOne({ where: { id: groupId } });
+    if (!group) {
+      throw new NotFoundException(`Group ${groupId} not found`);
+    }
+    if (group.type !== NodeType.GROUP) {
+      throw new BadRequestException(`Node ${groupId} is not a GROUP`);
+    }
+
+    const existing = await this.closureRepository.findOne({
+      where: { ancestorId: groupId, descendantId: userId },
+    });
+    if (existing) {
+      throw new ConflictException(
+        `User ${userId} already belongs to group ${groupId}`,
+      );
+    }
+
+    const groupAncestors = await this.closureRepository.find({
+      where: { descendantId: groupId },
+    });
+    if (groupAncestors.some((a) => a.ancestorId === userId)) {
+      throw new UnprocessableEntityException(`Cyclic relationship detected`);
+    }
+
+    const newLinks: ClosureEntity[] = [];
+
+    for (const ancestor of groupAncestors) {
+      newLinks.push(
+        this.closureRepository.create({
+          ancestorId: ancestor.ancestorId,
+          descendantId: userId,
+          depth: ancestor.depth + 1,
+        }),
+      );
+    }
+
+    const userSelf = await this.closureRepository.findOne({
+      where: { ancestorId: userId, descendantId: userId },
+    });
+    if (!userSelf) {
+      newLinks.push(
+        this.closureRepository.create({
+          ancestorId: userId,
+          descendantId: userId,
+          depth: 0,
+        }),
+      );
+    }
+
+    await this.closureRepository.save(newLinks);
   }
 }
