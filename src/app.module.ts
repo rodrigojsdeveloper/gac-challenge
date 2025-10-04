cdcccaeab0c237e00273361d7faa3d381d57516e
rodrigojsdeveloper <rodrigojsdeveloper@gmail.com>
2025-10-03 23:28:16 -0300

    Revert "feat: add entities"
    This reverts commit 4f4d60193df6c2045fd89b8768e1d582bdd8fa54.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,24 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './modules/users/users.module';
-import { NodeEntity } from './entities/node.entity';
-import { ClosureEntity } from './entities/closure.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +(process.env.DB_PORT || 5432),
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [NodeEntity, ClosureEntity],
       autoLoadEntities: true,
       synchronize: false,
     }),
-    UsersModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
