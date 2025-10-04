f70c61f86ad9f9129d5d573a34063612396d4dd0
rodrigojsdeveloper <rodrigojsdeveloper@gmail.com>
2025-10-03 23:38:30 -0300

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { NodeEntity } from './entities/node.entity';
+import { ClosureEntity } from './entities/closure.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
+      port: +(process.env.DB_PORT || 5432),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      entities: [NodeEntity, ClosureEntity],
       autoLoadEntities: true,
       synchronize: false,
     }),
+    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
